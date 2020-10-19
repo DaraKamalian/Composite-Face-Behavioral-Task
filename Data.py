43c9b01052462572d5af0c8929d26210ea377abf
Dara <app.tezolmarket@gmail.com>
2020-10-19 23:29:17 +0330

    "Committed"

--- Data.py
+++
@@ -1,7 +0,0 @@
-class ExcelFile:
-    def __init__(self, workbook, worksheet):
-        self.workbook = workbook
-        self.worksheet = worksheet
-
-    def get_data(self):
-        return [self.workbook, self.worksheet]
